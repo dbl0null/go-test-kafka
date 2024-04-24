04062526a17afa96da98dd16424343f8190e6fbc
Sergey Shiganov <dbl0null@gmail.com>
2024-04-24 14:47:49 +0300

    fix types and log error

--- producer/main.go
+++ producer/main.go
@@ -17,14 +17,14 @@ type Message struct {
 	Type       string    `json:"type"`
 	Timestamp  time.Time `json:"timestamp"`
 	Objects    []struct {
-		Id      string `json:"id"`
+		Id      int    `json:"id"`
 		Class   string `json:"class"`
 		Visible bool   `json:"visible"`
 		Bbox    struct {
-			Left   string `json:"left"`
-			Right  string `json:"right"`
-			Top    string `json:"top"`
-			Bottom string `json:"bottom"`
+			Left   int `json:"left"`
+			Right  int `json:"right"`
+			Top    int `json:"top"`
+			Bottom int `json:"bottom"`
 		} `json:"bbox"`
 	} `json:"objects"`
 }
@@ -35,6 +35,7 @@ func producerHandler(kafkaWriter *kafka.Writer) func(http.ResponseWriter, *http.
 
 		err := json.NewDecoder(req.Body).Decode(&m)
 		if err != nil {
+			fmt.Println(err.Error())
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
@@ -42,6 +43,7 @@ func producerHandler(kafkaWriter *kafka.Writer) func(http.ResponseWriter, *http.
 		topic := m.Type
 		value, err := json.Marshal(m)
 		if err != nil {
+			fmt.Println(err.Error())
 			http.Error(w, err.Error(), http.StatusBadRequest)
 		}
 
@@ -53,6 +55,7 @@ func producerHandler(kafkaWriter *kafka.Writer) func(http.ResponseWriter, *http.
 
 		err = kafkaWriter.WriteMessages(req.Context(), msg)
 		if err != nil {
+			fmt.Println(err.Error())
 			w.Write([]byte(err.Error()))
 			log.Fatalln(err)
 		}
@@ -72,7 +75,7 @@ func main() {
 
 	defer kafkaWriter.Close()
 
-	http.HandleFunc(":8080/api/v1/runner/events", producerHandler(kafkaWriter))
+	http.HandleFunc("/api/v1/runner/events", producerHandler(kafkaWriter))
 
 	fmt.Println("Listening on :8080/api/v1/runner/events...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
